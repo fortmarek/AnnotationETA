69cb3025989104c85bbf8dd952a5420f12eba2a3
fortmarek <marekfort@me.com>
2017-02-07 23:00:30 +0100

    shared AnnotationETA

---
+++ Example/AnnotationETA/AnnotationETA.h
@@ -0,0 +1,19 @@
+//
+//  AnnotationETA.h
+//  AnnotationETA
+//
+//  Created by Marek Fořt on 2/7/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnnotationETA.
+FOUNDATION_EXPORT double AnnotationETAVersionNumber;
+
+//! Project version string for AnnotationETA.
+FOUNDATION_EXPORT const unsigned char AnnotationETAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnnotationETA/PublicHeader.h>
+
+
